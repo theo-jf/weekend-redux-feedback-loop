ffeabda5692c38daa658278803ed4a138d11f9f6
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-23 15:41:45 -0500

    TODO list for Base-mode added

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,6 +2,33 @@ import React from 'react';
 import axios from 'axios';
 import './App.css';
 
+
+
+// TODO BASE-MODE:
+// Submitted component on load
+//       Conditional 'Welcome!' or 'Thank You for Your Submission!'
+//       Use the Store for this boolean --> changed on click in Review
+//       Leave new feedback button leads to Feedback component
+//
+// Feedback component houses all questions and inputs
+//       First route loaded is feedback/feeling
+//       Next brings us to feedback/understanding etc.
+//       Each next click stores that answer in Store
+//
+// When next button in the Comments component is clicked
+//       Display all answers from the Store in Review
+//       CANNOT EDIT OR GO BACK FOR BASE-MODE
+//            But, look up local storage in React/Redux
+//
+// In Review, once submit is clicked
+//       Submit an answer object through POST to the server / database
+//       Change that one boolean so 'Thanks' appears in Submitted
+//       Go back to submitted!
+
+// TODO STRETCH-MODE:
+// 
+
+
 function App() {
 
   return (
