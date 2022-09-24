571531c800ed8431e28c4dd4cd0b7f2f5482b65c
Theo Janke-Furman <tjankefurman@gmail.com>
2022-09-23 19:10:35 -0500

    Created header component and store

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,7 +7,7 @@ import { useDispatch, useSelector } from 'react-redux';
 // Router imports
 import { HashRouter as Router, Route, Link} from 'react-router-dom';
 // Component imports
-
+import Header from '../Header/Header';
 
 
 // TODO BASE-MODE:
@@ -57,10 +57,12 @@ function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Router>
+        <Header />
+        <Route exact path ="/">
+
+        </Route>
+      </Router>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -10,9 +10,27 @@ import { Provider } from 'react-redux';
 import logger from 'redux-logger';
 
 // Reducers below
+const message = (state = 'Welcome!', action) => {
+
+    switch (action.type) {
+        case 'SET_MESSAGE_WELCOME':
+            return 'Welcome!';
+        case 'SET_MESSAGE_THANKS':
+            return 'Thank you for your submission';
+    }
+    return state;
+}
 
 
 // Create Redux Store
+const storeInstance = createStore(
+    combineReducers(
+        {
+            message
+        }
+    ),
+    applyMiddleware(logger)
+)
 
 ReactDOM.render( 
     <Provider store={storeInstance}>
